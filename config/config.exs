2e5d018d75b766fd2f9b298f50d2f80cd50e62cc
Tetiana Dushenkivska <myemail@here>
2018-10-04 13:16:37 +0100

    Add postgres config required for eventstore
    Temporary add postgres config, so the tests can be run and test
    integration with EventStore.

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,10 @@
 use Mix.Config
+
+config :eventstore, EventStore.Storage,
+  serializer: EventStore.TermSerializer,
+  username: "postgres",
+  password: "postgres",
+  database: "eventstore_dev",
+  hostname: "localhost",
+  pool_size: 10,
+  pool_overflow: 5
