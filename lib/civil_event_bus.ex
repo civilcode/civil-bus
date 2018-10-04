581ad10cc378473b4271e6389289a74bf3effe5f
Tetiana Dushenkivska <myemail@here>
2018-10-04 11:58:21 +0100

    Create civil_event_bus application

---
+++ lib/civil_event_bus.ex
@@ -0,0 +1,18 @@
+defmodule CivilEventBus do
+  @moduledoc """
+  Documentation for CivilEventBus.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CivilEventBus.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
