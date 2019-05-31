62f76c308b6574f1803bea97e2f4f264277c21e9
Nicholas Henry <nicholas.henry@gmail.com>
2019-05-31 08:16:00 -0400

    feat: Make EventStore an optional dependency

--- lib/civil_bus/event_store.ex
+++ lib/civil_bus/event_store.ex
@@ -1,39 +1,41 @@
-defmodule CivilBus.EventStore do
-  @moduledoc """
-  EventStore implementation for the CivilBus.
-  """
+if Code.ensure_loaded?(EventStore) do
+  defmodule CivilBus.EventStore do
+    @moduledoc """
+    EventStore implementation for the CivilBus.
+    """
 
-  @behaviour CivilBus.Behaviour
+    @behaviour CivilBus.Behaviour
 
-  @impl true
-  def start_link(_opts \\ []) do
-    {:ok, :event_store}
-  end
+    @impl true
+    def start_link(_opts \\ []) do
+      {:ok, :event_store}
+    end
 
-  @impl true
-  def subscribe(channel) do
-    subscription_name = "subscription_#{inspect(self())}"
+    @impl true
+    def subscribe(channel) do
+      subscription_name = "subscription_#{inspect(self())}"
 
-    {:ok, _subscription} =
-      EventStore.subscribe_to_stream(to_string(channel), subscription_name, self())
+      {:ok, _subscription} =
+        EventStore.subscribe_to_stream(to_string(channel), subscription_name, self())
 
-    :ok
-  end
+      :ok
+    end
 
-  @impl true
-  def publish(channel, event) do
-    events = [
-      %EventStore.EventData{
-        event_type: to_string(event.__struct__),
-        data: event
-      }
-    ]
+    @impl true
+    def publish(channel, event) do
+      events = [
+        %EventStore.EventData{
+          event_type: to_string(event.__struct__),
+          data: event
+        }
+      ]
 
-    :ok = EventStore.append_to_stream(to_string(channel), :any_version, events)
-  end
+      :ok = EventStore.append_to_stream(to_string(channel), :any_version, events)
+    end
 
-  @impl true
-  def ack(_channel, _event) do
-    :ok
+    @impl true
+    def ack(_channel, _event) do
+      :ok
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule CivilBus.MixProject do
 
   defp deps do
     [
-      {:eventstore, "~> 0.16"},
+      {:eventstore, "~> 0.16", optional: true},
       {:poison, "~> 3.0"},
       {:mix_test_watch, "~> 0.5", only: :test, runtime: false}
     ]
