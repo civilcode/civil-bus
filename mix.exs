4f3f22230c9a6d37868a5157804288966655b747
Nicholas Henry <nicholas.henry@gmail.com>
2019-05-31 08:15:36 -0400

    chore: Upgrade Elixir 1.6 -> 1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CivilBus.MixProject do
     [
       app: :civil_bus,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env())
