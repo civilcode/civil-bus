a38dbc3534cf9d19e7ac0fed9a57440487860edb
Nicholas Henry <nicholas.henry@gmail.com>
2019-12-06 11:55:24 -0500

    test: Remove dependency on StorageCase for shared tests

--- test/support/shared_test_case.ex
+++ test/support/shared_test_case.ex
@@ -1,12 +1,35 @@
 defmodule CivilBus.SharedTestCase do
   @moduledoc false
+
+  defmacro __using__(_opts) do
+    quote do
+      import CivilBus.SharedTestCase
+    end
+  end
+
+  defmodule TestCase do
+    use ExUnit.CaseTemplate
+
+    using do
+      quote do
+        def assert_down(pid) do
+          ref = Process.monitor(pid)
+          assert_receive {:DOWN, ^ref, _, _, _}
+        end
+      end
+    end
+  end
+
+  @doc """
+  Define tests to be shared across other test cases.
+  """
   defmacro define_tests(do: block) do
     quote do
       defmacro __using__(options) do
         block = unquote(Macro.escape(block))
 
         quote do
-          use CivilBus.StorageCase
+          use TestCase
 
           @moduletag unquote(options)
           unquote(block)
